8fd7af19c0b1c704cdb972c700b7fefa9b1862fe
Crusbel <crusbeljl@gmail.com>
2017-02-13 14:29:39 -0500

    Filtro de palabras soeces

--- js/script.js
+++ js/script.js
@@ -3,12 +3,18 @@ var id_slider = "#my_slider";
 var slider_width_init = $(id_slider).width();
 var index_slider = 0; 
 
+/**
+ * Ejecuta la función para animar los campos
+ */
 input_focus('username');
 input_focus('email');
 input_focus('password');
 input_focus('message');
 input_focus('search');
 
+/**
+ * Formulario
+ */
 $('#my_form').submit(function(ev) { 
     ev.preventDefault();
 
@@ -36,22 +42,26 @@ $('#my_form').submit(function(ev) {
         data: formData,
         dataType: "json",
             success: function (response) {
-                console.log(response);
                 if (response.status_code === 0) {
+
                     var template = '<div class="modal_message">';
                         template += '<h4>Success !!</h4>';
                         template += '<p>Your Username :</p><p><span>'+response.data.username+'</span></p>';
                         template += '<p>Your Email :</p><p><span>'+response.data.email+'</span></p>';
                         template += '<p>Your Password :</p><p><span>'+response.data.password+'</span></p>';
                         template += '<p>Your Message :</p><p><span>'+response.data.message+'</span></p>';
-                        template += '</div>';     
+                        template += '</div>';
+
                     $('.modal').html(template).fadeIn('slow');  
+
                     clear_input();
+
                     $(document).mouseup(function (e){
-                            var container = $(".modal_message");
-                            if (!container.is(e.target) && container.has(e.target).length === 0)
-                                $(".modal_message").fadeOut('slow');                     
-                        });             
+                        var container = $(".modal_message");
+                        if (!container.is(e.target) && container.has(e.target).length === 0)
+                            $(".modal_message").fadeOut('slow');                     
+                    }); 
+
                 } else {
                     var template = '<div class="modal_message">';
                         template += '<h4>Houston, we have a problem !!</h4>';
@@ -63,6 +73,9 @@ $('#my_form').submit(function(ev) {
         });
 });
 
+/**
+ * Agrega las imagenes al slider
+ */
 $.ajax({
     type: "GET",
     url: "http://test.masuno.pe/images.php",
@@ -79,7 +92,9 @@ $.ajax({
 
 
 
-
+/**
+ * Slider
+ */
 function mySlider() {
     var slider_elements = 0;
     $(id_slider + " div").each(function () {
@@ -96,6 +111,9 @@ function animate_slider() {
     index_slider++;
 }
 
+/**
+ * Da efecto al 'label' cuando se pone el foco en el input
+ */
 function input_focus(input) {
     $('#'+input).blur(function () { 
         if ($(this).val() != '') {
@@ -106,23 +124,27 @@ function input_focus(input) {
     });
 }
 
+/**
+ * Campos requeridos
+ */
 function input_required(input) {
     var $selector = $('#'+input);
     var condition = false;
     
-        if ($selector.val() != '') {
-            if ($selector.val().length < 4) {
-                condition = false;
-            } else {
-                condition = true;
-            }
-        } else {
+    if ($selector.val() != '') {
+        if ($selector.val().length < 4)
             condition = false;
-        }
-
+        else
+            condition = true;
+    } else {
+        condition = false;
+    }
     return condition;
 }
 
+/**
+ *  Activa o desactiva el boton de envío
+ */
 function enabled_button(input1, input2, input3, input4) {
     var $button = $('#btn_submit');
     var regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
@@ -136,16 +158,48 @@ function enabled_button(input1, input2, input3, input4) {
     }  
 }
 
+/**
+ * Limpia los campos
+ */
 function clear_input() {
     $('.keyup_input').val('');
     $('.keyup_input').removeClass('focused');
     $('#btn_submit').addClass('disabled');
 }
 
+/**
+ * Ejecuta las funciones para validar los campos
+ */
 $('.keyup_input').on('keyup blur', function () {
     var username = input_required('username');
     var email = input_required('email');
     var password = input_required('password');
     var message = input_required('message');
    enabled_button(username, email, password, message); 
+});
+
+/**
+ * Filtro de palabras soeces
+ */
+var soeces_array = [];
+
+$.ajax({
+    type: "GET",
+    url: "json/soeces.json",
+    dataType: "json",
+    success: function (response) {
+        soeces_array = response.soeces;
+    }
+});
+
+$('.keyup_input').on('keyup blur',function(){
+    var cont = -1;
+
+    for (var i = 0; i < soeces_array.length; i++) {
+        cont = $(this).val().toLowerCase().indexOf(soeces_array[i]);
+            if (cont >= 0) {
+                $(this).val('');
+                break;
+            } 
+    }
 });
